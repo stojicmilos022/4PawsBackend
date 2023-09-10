a6c11ef6eed8d32b4a8c28e373a019d556c58124
stojicmilos022 <stojic.milos@gmail.com>
2023-09-10 20:02:34 +0200

    Added empty slike salona controler

---
+++ 4PawsBackend/Controllers/SlikeSalonaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4PawsBackend.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlikeSalonaController : ControllerBase
+    {
+    }
+}
